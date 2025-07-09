5ec59c2bef17add077b40fdc6a9a544a7d0244f9
FerryFL01 <ferryfebrian.lim@gmail.com>
2025-07-09 10:24:00 +0700

    fix: text styling on dark mode

--- src/pages/dashboard-cashier/index.tsx
+++ src/pages/dashboard-cashier/index.tsx
@@ -107,14 +107,14 @@ const DashboardCashier = () => {
                 ) :
                     <div className="flex gap-3 overflow-x-auto pb-2 mb-4 w-full">
 
-                        <Card className="hover:bg-gray-100 transition-colors p-3 shrink-0 gap-0 min-w-32 max-w-52 text-center flex justify-center items-center cursor-pointer" onClick={() => handleClick("Semua", "Semua")}>
+                        <Card className="hover:bg-gray-100 hover:text-black transition-colors p-3 shrink-0 gap-0 min-w-32 max-w-52 text-center flex justify-center items-center cursor-pointer" onClick={() => handleClick("Semua", "Semua")}>
                             <h2 className="text-base font-semibold">Semua</h2>
                             <p className="text-sm">{totalProducts} Products</p>
                         </Card>
 
                         {
                             categories?.filter((category) => category.status === true).map((category) => (
-                                <Card key={category.id} className="hover:bg-gray-100 transition-colors shrink-0 p-3 gap-1 min-w-32 max-w-52 text-center flex justify-center items-center cursor-pointer" onClick={() => handleClick(category.id, category.nama)}>
+                                <Card key={category.id} className="hover:bg-gray-100 hover:text-black transition-colors shrink-0 p-3 gap-1 min-w-32 max-w-52 text-center flex justify-center items-center cursor-pointer" onClick={() => handleClick(category.id, category.nama)}>
                                     <div className="w-full">
                                         <h2 className="text-base font-semibold line-clamp-1 break-words">{category.nama}</h2>
                                     </div>
@@ -227,7 +227,7 @@ const DashboardCashier = () => {
                     <div className="space-y-2 p-3">
                         {
                             selectedProduk.map((item) =>
-                                <div key={item.id} className="flex items-center justify-between bg-gray-100 p-3 rounded-lg">
+                                <div key={item.id} className="flex items-center justify-between bg-gray-100 text-black p-3 rounded-lg">
                                     <div className="flex flex-col">
                                         <span>{item.nama}</span>
                                         <span className="text-sm">Rp {item.harga} / Produk</span>
